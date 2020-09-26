96a053f592935063db6b420718e74080d90253ce
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-09-25 19:19:48 -0500

    authentication is working, need finishing touch

---
+++ src/javascripts/components/auth/auth.js
@@ -0,0 +1,50 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const signMeIn = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
+};
+
+// const loginButton = () => {
+//   const domString = `<div id="auth">
+//                       <span style="font-size: 5em; color: black;"><i class="fab fa-pinterest"></i></span>
+//                       <h4>Welcome to Pasture</h4>
+//                       <button id="google-auth" class="btn btn-primary btn-lg">
+//                         <i class="fab fa-google"></i></i>oogle Login
+//                       </button>
+//                     </div>`;
+
+//   $('#app').html(domString);
+//   $('#google-auth').on('click', signMeIn);
+// };
+
+const loginButton = () => {
+  const domString = `<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#"><span style="font-size: 2em; color: black;"><i class="fab fa-pinterest"></i></span></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" 
+  data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <ul class="navbar-nav mr-auto">
+  <li class="nav-item mx-3"  id="farmers-link">
+    <a class="nav-link" href="#"><i class="fas fa-chalkboard"></i> Boards</a>
+  </li>
+  <li class="nav-item mx-3" id="add-cow-link">
+    <a class="nav-link" href="#"><i class="fas fa-plus-circle"></i> Add A Pin</a>
+  </li>
+</ul>
+        <button id="google-auth" class="btn btn-primary btn-lg">
+        <i class="fab fa-google"></i></i>oogle Login
+        </button>
+    
+  </div>
+</nav>`;
+
+  $('#app').html(domString);
+  $('#google-auth').on('click', signMeIn);
+};
+
+export default { loginButton };
